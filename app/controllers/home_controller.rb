84a12ca84407e3c25c8dd7e8bd06b1377558220f
Dhani Himawan <dhnhimawan@gmail.com>
2022-05-20 11:19:20 +0700

    minor change

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,7 @@ class HomeController < ApplicationController
       @client_activity_processlist_late = ClientActivityProcesslist.where("target_start < ? AND status < 2", DateTime.now)
       @client_activity_processlist_upcoming = ClientActivityProcesslist.where("target_start >= ? AND target_start <= ? AND status < 2", DateTime.now, 7.days.from_now) 
   
-      @client_birthday = Client.where("cast(strftime('%m', birthdate) as int) = ?", DateTime.now.month)
+      @client_birthday = Client.where("cast(strftime('%m', birthdate) as int) = ? AND cast(strftime('%d', birthdate) as int) >= ?", DateTime.now.month, DateTime.now.day)
 
 
       @client_activity_certificate = ClientActivity.where("certificate_expired >= ? AND certificate_expired <= ?", DateTime.now, 90.days.from_now)
